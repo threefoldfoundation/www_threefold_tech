9d35765d0a6464ed8ecaa7be744023c684acac4e
Sacha <obeegads@incubaid.com>
2021-02-09 16:03:26 +0200

    reformat

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 
 
 set -e
-cd /Users/despiegk/codewww/github/threefoldtech/www_threefold_tech
+cd /Users/sacha/codewww/github/threefoldtech/www_threefold_tech
 
 #need to ignore errors for getting nvm not sure why
 set +e
-source /Users/despiegk/.publisher/nvm.sh
+source /Users/sacha/.publisher/nvm.sh
 
 set -e
 nvm use --lts
 
-export PATH=/Users/despiegk/.publisher/versions/node/v15.8.0/bin:$PATH
+export PATH=/Users/sacha/.publisher/versions/node/v15.8.0/bin:$PATH
 
 set +e
 gridsome build
 
 set -e
 
-mkdir -p /Users/despiegk/.publisher/publish/www_threefold_tech
-rsync -ra --delete /Users/despiegk/codewww/github/threefoldtech/www_threefold_tech/dist/ /Users/despiegk/.publisher/publish/www_threefold_tech/
+mkdir -p /Users/sacha/.publisher/publish/www_threefold_tech
+rsync -ra --delete /Users/sacha/codewww/github/threefoldtech/www_threefold_tech/dist/ /Users/sacha/.publisher/publish/www_threefold_tech/
 
-cd /Users/despiegk/codewww/github/threefoldtech/www_threefold_tech/dist
+cd /Users/sacha/codewww/github/threefoldtech/www_threefold_tech/dist
 
 #echo go to http://localhost:9999/
  #python3 -m http.server 9999
