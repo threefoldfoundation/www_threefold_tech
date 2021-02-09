ebf7f39f3e9b8f483dbff581370a3f376aa1e4e0
kristof <kristof@incubaid.com>
2021-02-09 04:51:27 +0200

    installer do

--- build.sh
+++ build.sh
@@ -22,6 +22,6 @@ rsync -ra --delete /Users/despiegk/codewww/github/threefoldtech/www_threefold_te
 
 cd /Users/despiegk/codewww/github/threefoldtech/www_threefold_tech/dist
 
-echo go to http://localhost:9999/
- python3 -m http.server 9999
+#echo go to http://localhost:9999/
+ #python3 -m http.server 9999
 
