474fb113a70ae4ffc9eebc189171814de73dd7d4
ehab-hassan <hassane@codescalers.com>
2021-10-12 12:38:06 +0200

    fix3

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout :hideHeader="true" :disableScroll="true">
-    <!-- <g-image
+    <g-image
       v-if="$page.markdownPage.solution_image"
       :src="$page.markdownPage.solution_image.src"
     />
 
-    <SolutionsHeader
+    <!-- <SolutionsHeader
       v-if="$page.markdownPage.header"
       :header="$page.markdownPage.header"
-    /> -->
+    />
 
     <Header
       :title="$page.markdownPage.header_title"
@@ -98,7 +98,7 @@
     <g-image
       v-if="$page.markdownPage.solution_image_5"
       :src="$page.markdownPage.solution_image_5.src"
-    />
+    /> -->
   </Layout>
 </template>
 
