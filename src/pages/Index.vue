55e0a180914784b22233aec6c4520ba78d5f0ddf
ehab-hassan <hassane@codescalers.com>
2021-10-12 13:52:10 +0200

    fix3

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -93,7 +93,7 @@
       :logos="$page.markdownPage.logos"
     /> -->
 
-    <CallToAction v-if="$page.markdownPage.cta" :cta="$page.markdownPage.cta" />
+    <!-- <CallToAction v-if="$page.markdownPage.cta" :cta="$page.markdownPage.cta" /> -->
 
     <g-image
       v-if="$page.markdownPage.solution_image_5"
